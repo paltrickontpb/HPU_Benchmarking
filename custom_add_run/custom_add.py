d525bdb8c2b0e992308251ad8a948bf3a1d9aba7
paltrickontpb <paltrickontpb@gmail.com>
2023-07-23 02:08:50 -0400

    First Revision

---
+++ custom_add_run/custom_add.py
@@ -0,0 +1,28 @@
++ ###############################################################################
+# Copyright (C) 2020-2021 Habana Labs, Ltd. an Intel Company
+###############################################################################
+
+import torch
+import os
+import habana_frameworks.torch.core
+
+custom_add_op_lib_path = "../build/src/add_lib.so"
+my_dir = os.path.realpath(__file__)
+my_len = my_dir.rfind('/')
+base_dir = my_dir[:my_len]
+torch.ops.load_library(os.path.join(base_dir, custom_add_op_lib_path))
+print(torch.ops.custom_op.custom_add_f32)
+
+a_cpu = torch.rand((6, 6))
+b_cpu = torch.rand((6, 6))
+c_cpu = torch.zeros((6, 6))
+
+a_hpu = a_cpu.to('hpu')
+b_hpu = b_cpu.to('hpu')
+c_hpu = c_cpu.to('hpu')
+
+c_hpu = torch.ops.custom_op.custom_add_f32(a_hpu, b,hpu)
+
+c_cpu = a_cpu + b_cpu
+
+assert(torch.equal(c_hpu.detach().cpu(), c_cpu.detach().cpu()))
