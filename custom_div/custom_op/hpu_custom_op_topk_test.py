38cda2f09fcd8ab2a442eb01af9d70e5f3a1d173
paltrickontpb <paltrickontpb@gmail.com>
2023-07-24 04:12:17 -0400

    CustomDivOp Test updated

--- custom_div/custom_op/hpu_custom_op_topk_test.py
+++ custom_div/custom_op/hpu_custom_op_topk_test.py
@@ -1,14 +1,23 @@
 import torch
 import habana_frameworks.torch.core
 
+valueA = 1456.0
+valueB = 32.0
+expected_division_result = valueA / valueB
+
 def test_custom_div_op_function(custom_op_lib_path):
     torch.ops.load_library(custom_op_lib_path)
     print(torch.ops.custom_op.custom_topk)
-    a_cpu = torch.rand((6, 6))
-    a_hpu = a_cpu.to('hpu')
-    a_topk_hpu, a_topk_indices_hpu = torch.ops.custom_op.custom_topk(a_hpu, 3, 1, False)
-    a_topk_cpu, a_topk_indices_cpu = a_cpu.topk(3, 1)
-    assert(torch.equal(a_topk_hpu.detach().cpu(), a_topk_cpu.detach().cpu()))
+    a_cpu = valueA
+    b_cpu = valueB
+    c_cpu = expected_division_result
 
-libpath = ""
+    a_hpu = a_cpu.to('hpu')
+    b_hpu = b_cpu.to('hpu')
+    c_hpu = torch.ops.custom_op.custom_div(a_hpu, b_hpu)
+    
+    assert(torch.equal(c_hpu.detach().cpu(), c_cpu.detach().cpu()))
+
+libpath = "./build/"
+test_custom_div_op_function(libpath)
 
